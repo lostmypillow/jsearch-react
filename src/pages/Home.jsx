3a5d44d66631f661abb1ecc4ffdbb3a674fda0aa
Johnny Lin <47119111+lostmypillow@users.noreply.github.com>
2024-04-26 20:18:17 +0800

    finished movie search functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,10 @@
 import React from 'react'
-import { useContext, createContext, useState } from 'react'
+import { useContext, createContext, useState, useEffect } from 'react'
 import { useNavigate, Outlet } from 'react-router-dom'
 import { useAutoAnimate } from '@formkit/auto-animate/react'
+import { check } from 'prettier'
 const ExpandedContext = createContext(true)
 const SearchContext = createContext("")
-
-function CloseButton({ toggleClose }) {
-    const [close, setClose] = useState(false)
-    const expandedState = useContext(ExpandedContext)
-    function sendCloseToParent() {
-        toggleClose(close)
-    }
-    return (
-        <button onClick={sendCloseToParent} className='btn-primary md:hidden'>
-            {expandedState ? "Close" : "Search"}
-        </button>
-    )
-}
 
 function BarLeft({ children }) {
     return (
@@ -30,9 +18,17 @@ function BarLeft({ children }) {
     )
 }
 
-function BarMid({updateSearchTerm}) {
+
+
+
+
+
+
+function BarCombined() {
+    const navigate = useNavigate()
     const [searchType, setsearchType] = useState('movies')
     const [searchTerm, setsearchTerm] = useState('')
+
     function changeToMovies() {
         setsearchType("movies")
     }
@@ -41,104 +37,205 @@ function BarMid({updateSearchTerm}) {
     }
     function passSearchTerm(event) {
         setsearchTerm(event.target.value)
-        updateSearchTerm(event.target.value)
     }
-    return (
-        <div className='flex flex-col md:flex-row items-center justify-center w-full grow gap-2'>
-            <div className='flex flex-row'>
-                <button
-                    onClick={changeToMovies}
-                    className={`${searchType == 'movies' ? "bg-black text-white rounded-full px-4 py-2 w-full" : "px-4 py-2 w-full"}`}>
-                    Movies
-                </button>
+    const { movies, setMovies } = useContext(SearchContext)
+    const { expanded, setexpanded } = useContext(ExpandedContext)
+    function checkContext() {
+        setMovies(results)
+        navigate("search")
 
-                <button
-                    onClick={changeToGoogle}
-                    className={`${searchType == 'google' ? "bg-black text-white rounded-full px-4 py-2 w-full" : "px-4 py-2 w-full"}`}>
-                    Google
-                </button>
+    }
+
+    const fetchAndNav = async () => {
+        if (searchType == 'movies') {
+            try {
+                const response = await axios.get('https://www.omdbapi.com/?apikey=e9cb394' + '&s=' + searchTerm);
+                console.log(response.data.Search)
+                // const mock = [
+                //     {
+                //       "Title": "Dune",
+                //       "Year": "2021",
+                //       "imdbID": "tt1160419",
+                //       "Type": "movie",
+                //       "Poster": "https://m.media-amazon.com/images/M/MV5BMDQ0NjgyN2YtNWViNS00YjA3LTkxNDktYzFkZTExZGMxZDkxXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg"
+                //     },
+                //     {
+                //       "Title": "Dune: Part Two",
+                //       "Year": "2024",
+                //       "imdbID": "tt15239678",
+                //       "Type": "movie",
+                //       "Poster": "https://m.media-amazon.com/images/M/MV5BN2QyZGU4ZDctOWMzMy00NTc5LThlOGQtODhmNDI1NmY5YzAwXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_SX300.jpg"
+                //     },
+                //     {
+                //       "Title": "Dune",
+                //       "Year": "1984",
+                //       "imdbID": "tt0087182",
+                //       "Type": "movie",
+                //       "Poster": "https://m.media-amazon.com/images/M/MV5BYTAzYzNlMDMtMGRjYS00M2UxLTk0MmEtYmE4YWZiYmEwOTIwL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNzc5MjA3OA@@._V1_SX300.jpg"
+                //     },
+                //     {
+                //       "Title": "Jodorowsky's Dune",
+                //       "Year": "2013",
+                //       "imdbID": "tt1935156",
+                //       "Type": "movie",
+                //       "Poster": "https://m.media-amazon.com/images/M/MV5BMTU0MzcxMTAxMl5BMl5BanBnXkFtZTgwODMyMTIxMTE@._V1_SX300.jpg"
+                //     },
+                //     {
+                //       "Title": "Dune",
+                //       "Year": "2000",
+                //       "imdbID": "tt0142032",
+                //       "Type": "series",
+                //       "Poster": "https://m.media-amazon.com/images/M/MV5BMTU4MjMyMTkxN15BMl5BanBnXkFtZTYwODA5OTU5._V1_SX300.jpg"
+                //     },
+                //     {
+                //       "Title": "Children of Dune",
+                //       "Year": "2003",
+                //       "imdbID": "tt0287839",
+                //       "Type": "series",
+                //       "Poster": "https://m.media-amazon.com/images/M/MV5BMTI3ODIwMTQ5OF5BMl5BanBnXkFtZTYwNTkwNDk5._V1_SX300.jpg"
+                //     },
+                //     {
+                //       "Title": "Dune Drifter",
+                //       "Year": "2020",
+                //       "imdbID": "tt11835714",
+                //       "Type": "movie",
+                //       "Poster": "https://m.media-amazon.com/images/M/MV5BMjc4ODFiNzItZWY5OS00YzZkLWE0ZmYtMDYwODZlMDY0NjhhXkEyXkFqcGdeQXVyMDA4NzMyOA@@._V1_SX300.jpg"
+                //     },
+                //     {
+                //       "Title": "Planet Dune",
+                //       "Year": "2021",
+                //       "imdbID": "tt15331462",
+                //       "Type": "movie",
+                //       "Poster": "https://m.media-amazon.com/images/M/MV5BYzVkNjZlMDctODEzMy00MWE1LWI4YTUtZjdmNDZhYmZhODI0XkEyXkFqcGdeQXVyNTkzMzg3NDM@._V1_SX300.jpg"
+                //     },
+                //     {
+                //       "Title": "Dune Warriors",
+                //       "Year": "1991",
+                //       "imdbID": "tt0099474",
+                //       "Type": "movie",
+                //       "Poster": "https://m.media-amazon.com/images/M/MV5BNjU4NDc4ZTQtZGI1Ny00NTc4LTk4ODctOTIyZTliZmM0MmJjXkEyXkFqcGdeQXVyNjExODE1MDc@._V1_SX300.jpg"
+                //     },
+                //     {
+                //       "Title": "Dune World",
+                //       "Year": "2021",
+                //       "imdbID": "tt14450978",
+                //       "Type": "movie",
+                //       "Poster": "https://m.media-amazon.com/images/M/MV5BMzMxMzQ4MTMtYTk4MC00Njc3LTllN2MtOThjMzg4OTgwNDNhXkEyXkFqcGdeQXVyMjc0Nzk1Mg@@._V1_SX300.jpg"
+                //     }
+                //   ]
+                await setMovies(response.data.Search)
+                await setexpanded(false)
+                navigate("search")
+
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        } else {
+            const options = {
+                method: 'GET',
+                url: 'https://google-search74.p.rapidapi.com/',
+                params: {
+                    query: 'Nike',
+                    limit: '10',
+                    related_keywords: 'false'
+                },
+                headers: {
+                    'X-RapidAPI-Key': '4cc2304fb3msh28544a85c39f266p17c92bjsnd635c2ee140a',
+                    'X-RapidAPI-Host': 'google-search74.p.rapidapi.com'
+                }
+            };
+            try {
+                const response = await axios.request(options);
+             console.log(response)
+
+            } catch (error) {
+                console.log("Error")
+            }
+        }
+
+
+    }
+
+
+
+    return (
+        <>
+            <div className='flex flex-col md:flex-row items-center justify-center w-full grow gap-2'>
+                <div className='flex flex-row'>
+                    <button
+                        onClick={changeToMovies}
+                        className={`${searchType == 'movies' ? "bg-black text-white rounded-full px-4 py-2 w-full" : "px-4 py-2 w-full"}`}>
+                        Movies
+                    </button>
+
+                    <button
+                        onClick={changeToGoogle}
+                        className={`${searchType == 'google' ? "bg-black text-white rounded-full px-4 py-2 w-full" : "px-4 py-2 w-full"}`}>
+                        Google
+                    </button>
+
+                </div>
+
+                <input
+                    value={searchTerm}
+                    onChange={passSearchTerm}
+                    className='border-2 border-black px-4 py-2 w-[90%] rounded-xl'
+                    type="text"
+                    placeholder={searchType == 'movies' ? 'Search movies' : 'Search google'}
+                    autoFocus />
 
             </div>
 
-            <input value={searchTerm} onChange={passSearchTerm} className='border-2 border-black px-4 py-2 w-[90%] rounded-xl' type="text" placeholder={searchType == 'movies'? 'Search movies' : 'Search google'} autoFocus />
-{searchTerm}
 
-        </div>
-    )
-}
-
-function BarRight() {
-    const searchTerm = useContext(SearchContext)
-    const navigate = useNavigate()
-    const [movies, setMovies] = useState()
-    function navigateToSearch() {
-
-        navigate(`/search`)
-    }
-    async function searchMovies() {
-        try {
-            const response = await axios.get('https://www.omdbapi.com/?apikey=e9cb394' + '&s=' + searchTerm);
-          
-            console.log(response.data.Search)
-        
-            setMovies()
-            console.log(movies)
-         
-        } catch (error) {
-            // movieError.value = response.data.Error
-        }
-    }
-    return (
-        <div className='flex w-full grow md:w-fit items-center justify-center'>
-            <button onClick={searchMovies} className='btn-primary'>
-    search
-                {searchTerm}
-              
-            </button>
-        </div>
+            <div className='flex w-full grow md:w-fit items-center justify-center'>
+                <button onClick={fetchAndNav} className='btn-primary'>
+                    search
+                </button>
+            </div>
+        </>
     )
 }
 
 
-
-function MobileBar({ children }) {
-    const expandedState = useContext(ExpandedContext)
+function MobileBar() {
+    const [expanded, setexpanded] = useState(true)
+    function collapse() {
+        setexpanded(!expanded)
+    }
     return (
-        <div className={`flex flex-col md:flex-row items-center justify-center w-screen z-20 fixed bottom-0 md:top-0 left-0  p-4 rounded-t-xl  ${expandedState ? "h-[50vh] md:h-11" : ""} `} >
-            {children}
+        <div className={`flex flex-col md:flex-row items-center justify-center w-screen z-20 fixed bottom-0 md:top-0 left-0  p-4 rounded-t-xl border-2 border-black bg-white  ${expanded ? "h-[50vh] md:h-11" : ""} `} >
+            <BarLeft>
+                <button onClick={collapse} className='btn-primary md:hidden'>
+                    {expanded ? "Close" : "Search"}
+                </button>
+            </BarLeft>
+
+            <ExpandedContext.Provider value={{ expanded, setexpanded }}>
+                {expanded ?
+                    <>
+                        <BarCombined />
+                    </> :
+                    <></>}
+            </ExpandedContext.Provider>
         </div>
     )
 }
 
 
 export default function Home() {
-    const [expanded, setexpanded] = useState(true)
-    const [searchTerm, setsearchTerm] = useState("")
-    function toggleClose(data) {
-        setexpanded(!expanded)
-    }
-    function updateSearchTerm(data) {
-        setsearchTerm(data)
-    }
+
+    const [movies, setMovies] = useState(null)
+
+
     return (
         <>
-            <Outlet movieArray={[1, 2]} />
-            <ExpandedContext.Provider value={expanded}>
-                <MobileBar>
-                    <BarLeft>
-                        <CloseButton toggleClose={toggleClose} />
-                    </BarLeft>
+            <SearchContext.Provider value={{ movies, setMovies }}>
+                <Outlet context={movies} />
+                <button onClick={() => {
 
-                    {expanded ?
-                        <>
-                        <SearchContext.Provider value={searchTerm}>
-                            <BarMid updateSearchTerm={updateSearchTerm}/>
-                            <BarRight />
-                            </SearchContext.Provider>
-                        </> :
-                        <></>}
-                </MobileBar>
-            </ExpandedContext.Provider>
+                }}>setContext</button>
+                <MobileBar />
+            </SearchContext.Provider>
         </>
 
     )
--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
+import { useOutletContext } from 'react-router';
 
 
 function MovieCard({ movie }) {
   return (
-    <div className="flex flex-row items-center justify-start gap-4 border-2 border-black min-h-24 m-2 p-2 rounded-2xl backdrop-blur-3xl grow md:max-w-96">
+    <div classNameName="flex flex-row items-center justify-start gap-4 border-2 border-black min-h-24  px-4 py-2 rounded-2xl w-full md:w-96">
 
       <img
-        className='h-11'
+        classNameName='h-11'
         src={movie.Poster != "N/A" ? movie.Poster : "{ SearchIcon }"}
         alt="poster"
       />
-      <h3 className="text-sm font-light">
+      <h3 classNameName="text-sm font-light">
         {movie.Type.toUpperCase()}
       </h3>
 
-      <div className="text-xl font-bold">
-        {movie.Title} <br class="md:hidden"></br>
-        <div className="text-base font-semibold">
+      <div classNameName="text-xl font-bold">
+        {movie.Title} <br classNameName="md:hidden"></br>
+        <div classNameName="text-base font-semibold">
           ({movie.Year})
         </div>
 
@@ -27,12 +28,77 @@ function MovieCard({ movie }) {
   );
 }
 
-export default function Results({ movieArray }) {
+function KnowledgeCard({knowledge_panel}) {
   return (
-    <div className="flex flex-col md:pt-11 w-screen mx-4 my-2 gap-2">
+    <>
+   <div
+   className="flex flex-col items-start justify-center gap-2 border-2 border-black min-h-24  mx-4 my-8 p-4 rounded-3xl backdrop-blur-3xl grow">
+   <h3 className="text-sm font-light">KNOWLEDGE PANEL</h3>
 
-      <div className='font-bold text-3xl'>    Results</div>
+   <div className="flex flex-row w-full items-center justify-between">
+      <h1 className="text-2xl">
+         { knowledge_panel.name }
+      </h1>
+      <img className="max-w-10"alt="image" />
+   </div>
 
+   <h2 className="text-base font-light">
+         { knowledge_panel.label }
+      </h2>
+
+
+   <div className="text-base">
+      &emsp;{ knowledge_panel.description.text }
+   </div>
+
+   <div className="text-sm">
+      Source:
+      <Link to={knowledge_panel.description.url}>
+         {{ knowledge_panel.description.site }}
+      </Link>
+   </div>
+   <br />
+   <button>
+      <div className=" flex flex-row" >
+      
+         Show More Info
+      </div>
+      <div className=" flex flex-row" >
+         <MinusSVG />
+         Show Less Info
+      </div>
+   </button>
+
+   <div v-auto-animate>
+      <div  className="flex flex-col gap-4">
+         <div >
+
+
+            <div className="font-bold"> { inf.title }:</div>
+
+            <div >
+               { label }
+            </div>
+         </div>
+      </div>
+
+   </div>
+</div>
+</>
+  )
+}
+
+
+export default function Results() {
+  const movieArray = useOutletContext()
+  return (
+    <div classNameName="flex flex-col md:pt-14 w-svw px-4 py-2 gap-2">
+
+      <div classNameName='font-bold text-3xl'>
+        Results:
+      </div>
+
+<div classNameName='flex flex-col md:flex-row md:flex-wrap gap-4 items-center justify-center w-full'>
       {movieArray?.length > 0 ? (
         movieArray.map((movie) => (
           <MovieCard key={movie.imdbID} movie={movie} />
@@ -40,6 +106,7 @@ export default function Results({ movieArray }) {
       ) : (
         <h1>No Results</h1>
       )}
+</div>
 
     </div>
   )
